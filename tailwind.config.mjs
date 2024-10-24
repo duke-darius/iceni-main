a92b80e940ffa423dd71a96d320bd059eb508eba
aaron <aaron.griffin@facepunchstudios.com>
2024-10-24 11:17:12 +0100

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                primary: ['Poppins', 'system-ui', 'sans-serif'],
+                display: ['Space Grotesk', 'system-ui', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                primary: ['Poppins', 'system-ui', 'sans-serif'],
+                display: ['Space Grotesk', 'system-ui', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
